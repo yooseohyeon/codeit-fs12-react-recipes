fa7b52bf3b96118cd5a8f40aded3fa890067089e
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-20 23:17:48 +0900

    feat: 레시피별 재료 추가 및 중복 입력 방지 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,48 @@ const App = () => {
       ingredients: ["부대", "물", "고춧가루", "두부", "양파"],
     },
   ]);
+  const [newIngredient, SetNewIngredients] = useState("");
+
+  const handleAdd = (id) => {
+    if (!newIngredient.trim()) return;
+
+    setRecipes(
+      recipes.map((recipe) => {
+        if (recipe.id === id) {
+          const exists = recipe.ingredients.includes(newIngredient);
+
+          if (exists) return recipe;
+
+          return {
+            ...recipe,
+            ingredients: [...recipe.ingredients, newIngredient],
+          };
+        } else {
+          return recipe;
+        }
+      }),
+    );
+  };
 
   return (
     <div>
       <h1>레시피</h1>
       <ul>
-        {recipes.map((r) => (
-          <li key={r.id}>
-            <h3>{r.name}</h3>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <h3>{recipe.name}</h3>
+            <input
+              type="text"
+              name="ingredient"
+              value={newIngredient}
+              placeholder="재료를 입력하세요."
+              onChange={(e) => SetNewIngredients(e.target.value)}
+            ></input>
+            <button type="submit" onClick={() => handleAdd(recipe.id)}>
+              제출
+            </button>
             <ul>
-              {r.ingredients.map((ingredient, idx) => (
+              {recipe.ingredients.map((ingredient, idx) => (
                 <li key={idx}>{ingredient}</li>
               ))}
             </ul>
